844f57c2b30bba7bd55e1a3c8830f2ff2e7785f6
Pavlo <maks.chudin567@gmail.com>
2025-05-02 12:49:49 +0300

    small-refactor-versel

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { getHomePage } from "../../../api/getHomePage";
 import "./HomePage.scss";
 import { AnimeSection } from "../../../components/shared/AnimeSection/AnimeSection";
