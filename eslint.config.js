83b2011d1d75588e58d77906b7fee6c07a7df2ed
Pavlo <maks.chudin567@gmail.com>
2025-12-25 02:49:25 +0100

    refresh: eslint

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import nextPlugin from "@next/eslint-plugin-next";
+
+export default [
+  {
+    ignores: [
+      "node_modules",
+      ".next",
+      "dist",
+      "build",
+      "prisma",
+      "tailwind.config.js",
+      "postcss.config.js",
+      "eslint.config.js"
+    ]
+  },
+  js.configs.recommended,
+  nextPlugin.configs.recommended
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}", // или "./app/**/*..." если без src
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
