3aa062790973f2fe1efe481bebee6cc20b360fcd
Pavlo <maks.chudin567@gmail.com>
2026-03-04 02:55:19 +0100

    fix: Pagination-GenderData

--- src/app/anime/category/[gender]/_components/GenderData.tsx
+++ src/app/anime/category/[gender]/_components/GenderData.tsx
@@ -33,7 +33,7 @@ export default async function GenderData({ gender, page }: Props) {
           ))}
         </AnimeListLayout>
 
-        <Pagination pageCount={totalPages} currentPage={page} />
+        <Pagination pageCount={totalPages ?? 1} currentPage={page} />
       </div>
     );
   } catch (error) {
